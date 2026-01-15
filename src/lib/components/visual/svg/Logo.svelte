a712f80c5c11427a78137fefb52afdaf9c183d84
xiongfh2008 <xiongfh2000@sina.com>
2026-01-16 00:59:53 +0800

    20260116更新LOGO

--- src/lib/components/visual/svg/Logo.svelte
+++ src/lib/components/visual/svg/Logo.svelte
@@ -1,26 +1,26 @@
 <script>
 	// VERT Logo
 	export let className = "";
-	export let width = "100%";
-	export let height = "100%";
 </script>
 
-<svg
-	width={width}
-	height={height}
-	viewBox="0 0 3000 3000"
-	fill="none"
-	xmlns="http://www.w3.org/2000/svg"
-	xmlns:xlink="http://www.w3.org/1999/xlink"
-	class={className}
-	preserveAspectRatio="xMidYMid slice"
->
-	<image
-		href="/VERT_black_on_white.svg"
-		width="100%"
-		height="100%"
-		x="0"
-		y="0"
-		preserveAspectRatio="xMidYMid slice"
-	/>
-</svg>
+<div class={className} style="display: flex; justify-content: center; align-items: center;">
+	<svg
+		width="50%"
+		height="50%"
+		viewBox="0 0 1638 1638"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		preserveAspectRatio="xMidYMid meet"
+		style="width: 50%; height: 50%;"
+	>
+		<image
+			href="/VERT_black_on_white.svg"
+			width="100%"
+			height="100%"
+			x="0"
+			y="0"
+			preserveAspectRatio="xMidYMid meet"
+		/>
+	</svg>
+</div>
